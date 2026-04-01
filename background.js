f4e1f876bb07b52022976847fe9233822f03e2b2
totuomas <to.tuomas@outlook.com>
2026-04-01 17:56:39 +0300

    badge and icon

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
+//background.js
+chrome.action.setBadgeText({ text: "off" });
+chrome.action.setBadgeBackgroundColor({ color: "#646464" });
+
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   if (msg.type === "GET_TAB_ID") {
     sendResponse(sender.tab.id);
